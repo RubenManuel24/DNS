e3564b86e21c3bd1472e3b9457221011b762381f
Ruben Manuel <rubensapalo97@gmail.com>
2024-12-06 12:06:22 +0100

    App finalizado

--- lib/src/repository/http_service.dart
+++ lib/src/repository/http_service.dart
@@ -1,11 +1,10 @@
 import 'dart:developer';
-import 'dart:io';
-import 'package:EDS/src/feacture/home/home_scren.dart';
-import 'package:EDS/src/model/user_model.dart';
-import 'package:EDS/src/ui/loader_animation.dart';
-import 'package:EDS/src/ui/message.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:EDS/src/ui/message.dart';
+import 'package:EDS/src/model/user_model.dart';
+import 'package:EDS/src/ui/loader_animation.dart';
+import 'package:EDS/src/feacture/home/home_scren.dart';
 
 class HttpService {
   Future<void> sendData(BuildContext context, UserModel user) async {
@@ -17,14 +16,13 @@ class HttpService {
         return const LoaderAnimation(); // Exibir animação de carregamento
       },
     );
-
     try {
       // Criar a requisição multipart
       var request = http.MultipartRequest(
         'POST',
-        Uri.parse('http://172.22.38.16:3333/clientes'), // Substituir pela URL correta
+        Uri.parse(
+            'http://172.22.38.16:3333/clientes'), // Substituir pela URL correta
       );
-
       // Adicionar campos de texto ao formulário
       request.fields['primeiro_nome'] = user.nomeCompleto;
       request.fields['bi_identidade'] = user.numeroBI;
@@ -37,6 +35,7 @@ class HttpService {
       request.fields['local_emissao'] = user.localEmissao;
 
       // Adicionar a foto principal (personFotos) como arquivo
+
       if (user.personFotos.isNotEmpty) {
         request.files.add(
           await http.MultipartFile.fromPath(
@@ -45,8 +44,7 @@ class HttpService {
           ),
         );
       }
-
-       if (user.personFotos.isNotEmpty) {
+      if (user.personFotos.isNotEmpty) {
         request.files.add(
           await http.MultipartFile.fromPath(
             'doc_front', // Nome esperado pelo back-end
@@ -54,9 +52,7 @@ class HttpService {
           ),
         );
       }
-
-
-       if (user.personFotos.isNotEmpty) {
+      if (user.personFotos.isNotEmpty) {
         request.files.add(
           await http.MultipartFile.fromPath(
             'doc_back', // Nome esperado pelo back-end
@@ -67,23 +63,18 @@ class HttpService {
 
       // Enviar a requisição
       var response = await request.send();
-
       // Fechar o loader após a resposta
       Navigator.pop(context);
-
       if (response.statusCode == 200) {
         Message.showSucess("Dados enviados com sucesso!", context);
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => HomeScreen()
-            ),
+            builder: (context) => HomeScreen(),
+          ),
         );
       } else {
-        Message.showErro(
-          "Falha ao enviar os dados",
-          context,
-        );
+        Message.showErro("Falha ao enviar os dados", context);
       }
     } catch (e, s) {
       // Fechar o loader em caso de erro
